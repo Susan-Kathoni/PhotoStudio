ed19f288299999ff12132bc4973ded16a3a16e24
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-11-15 20:36:30 +0300

    Add imports and create a view

--- galleryPhotos/views.py
+++ galleryPhotos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+import datetime as dt
+from gallery.models import Image, Category, Location
+
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to PhotoStudio')
